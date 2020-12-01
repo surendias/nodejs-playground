bbbb56f505792ff04311b0a8877edb3e934f7dd6
Suren Dias <suren.dias87@gmail.com>
2020-12-01 09:06:39 +0530

    javascript promises

---
+++ promises.js
@@ -0,0 +1,11 @@
+let done = true;
+
+const isItDoneYet = new Promise((resolve, reject) => {
+  if (done) {
+    const workDone = "Here is the thing I built";
+    resolve(workDone);
+  } else {
+    const why = "Still working on something else";
+    reject(why);
+  }
+});
